9367f141e0c4de96c321a4396d61313d312f081c
Макс <mratnikov@gmail.com>
2018-07-29 20:06:32 +0300

    This my second commit

---
+++ Elif.py
@@ -0,0 +1,11 @@
+age = int(input())
+if age < 0:
+    print ('ne rodylsa')
+elif age <= 3:
+    print ('mladenec')
+elif age <= 10:
+    print ('dete')
+elif age <= 14:
+        print ( ' dont know what it')
+else:
+    print ('vzroslyi')
