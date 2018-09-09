1226b722176a4015018ce1f873b774740efe27c5
Макс <mratnikov@gmail.com>
2018-09-09 05:17:47 +0300

    This my second commit

--- perebor.py
+++ perebor.py
@@ -1,4 +1,5 @@
-for i in range(2):
-    for j in range(2):
-        for k in range(2):
-print(i, j, k, sep = ' ')
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            for f in range(3):
+                print(i, j, k, f, sep = ' ')
